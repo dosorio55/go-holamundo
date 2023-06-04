82b8862c29ec7b7e5f3fa4bedceeda75672a81c3
Diego Osorio <dosorio55@gmail.com>
2023-06-04 10:44:06 +0200

    added ifs

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import ("github.com/dosorio55/hola-mundo/variables")
+import (
+	"fmt"
+	"github.com/dosorio55/hola-mundo/variables"
+	"runtime"
+)
 
 func main() {
 	variables.ShowIntegers()
+	variables.CreatePerson()
+
+	state, newString := variables.ConvertNumberToString(10023216546546)
+	fmt.Println(state, newString)
+
+	os := runtime.GOOS
+
+	// this version of the if assaigns the variable in the same line of the condition then creates the condition
+	/* 	if os := runtime.GOOS; os == "Linux." {
+	   	} else if os == "Windows" {
+
+	   	} */
+
+	switch os {
+	case "Linux":
+		fmt.Println("Linux")
+	case "Windows":
+		fmt.Println("windows")
+	default:
+		fmt.Println("No se reconoce el sistema operativo")
+	}
 }
---
+++ upload.bat
@@ -0,0 +1,3 @@
+git add -A
+git commit -m "added ifs"
+git push
